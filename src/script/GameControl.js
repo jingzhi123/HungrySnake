4f300a548402de8d84311b1b7fdae4b3975c21f6
jingzhi123 <abc231@qq.com>
2020-08-30 20:34:14 +0800

    update

--- src/script/GameControl.js
+++ src/script/GameControl.js
@@ -23,6 +23,30 @@ export default class GameControl extends BaseScript {
         let ani;
     }
     onAwake(){
+        console.log('是否微信小游戏',Laya.Browser.onMiniGame);
+        if(!Laya.Browser.onMiniGame){
+            wx = Laya.Browser.window.wx;
+        }
+        //登陆按钮
+        let button = wx.createUserInfoButton({
+            type:'text',
+            text:'登陆!',
+            style: {
+                left: wx.getSystemInfoSync().windowWidth/2-100,
+                top: 76,
+                width: 200,
+                height: 40,
+                lineHeight: 40,
+                backgroundColor: '#ff0000',
+                color: '#ffffff',
+                textAlign: 'center',
+                fontSize: 16,
+                borderRadius: 4
+                }
+            })
+        button.onTap((res) => {
+            console.log(res)
+        })
         //开始按钮点击
         this.startBtn.clickHandler = Laya.Handler.create(this,(e)=>{
             this.onStartBtnClick()
